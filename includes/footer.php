567a898aebe2abb3007e6b93b10fe0963014c1f8
chirchir12 <chirchir7370@gmail.com>
2020-10-22 12:42:34 +0300

    update index page

---
+++ includes/footer.php
@@ -0,0 +1,86 @@
+ <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-2 col-md-6 footer-links">
+            <h4>Useful Links</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Pricing</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Discounts</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Reviews</a></li>
+
+            </ul>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Our Services</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Terms of Service</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Cookie policy</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Refund policy</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-4 col-md-6 footer-contact">
+            <h4>Contact Us</h4>
+            <p>
+              A108 Adam Street <br>
+              New York, NY 535022<br>
+              United States <br><br>
+              <strong>Phone:</strong> +1 938 839 9820<br>
+              <strong>Email:</strong> info@computerprogramminghelp.com<br>
+            </p>
+
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-info">
+            <h3>About CPH</h3>
+            <p>At ComputerProgrammingHelp, we have a team of experienced and dedicated tutors who deliver your work with the set deadline, all at an affordable cost. Our experience in the field spans more than 10 years. We deliver quality and original solutions on time to students, ensuring that they get the good grades they deserve.</p>
+            <div class="social-links mt-3">
+              <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+              <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+              <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+              <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+              <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="copyright">
+        Copyright &copy; 2020 <strong><span>Academic Aiders Inc.</span></strong>
+      </div>
+      <div class="credits">
+        All Rights Reserved
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/jquery/jquery.min.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/jquery-sticky/jquery.sticky.js"></script>
+  <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+  <script src="assets/vendor/waypoints/jquery.waypoints.min.js"></script>
+  <script src="assets/vendor/counterup/counterup.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/venobox/venobox.min.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</php>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,213 @@
+<!DOCTYPE php>
+<php lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Computer Programming Help - Home</title>
+  <meta content="" name="descriptison">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/logo.png" rel="icon">
+  <!-- <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon"> -->
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- Start of Async Callbell Code -->
+<script>
+  window.callbellSettings = {
+    token: "V14Gb4sKwfJhUfvkY1ATEcWj"
+  };
+</script>
+<script>
+  (function(){var w=window;var ic=w.callbell;if(typeof ic==="function"){ic('reattach_activator');ic('update',callbellSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Callbell=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://dash.callbell.eu/include/'+window.callbellSettings.token+'.js';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()
+</script>
+<!-- End of Async Callbell Code -->
+</head>
+
+<body>
+
+  <!-- ======= Top Bar ======= -->
+  <section id="topbar" class="d-none d-lg-block">
+    <div class="container d-flex">
+      <div class="contact-info mr-auto">
+        <i class="icofont-envelope"></i><a href="mailto:info@computerprogramminghelp.com">info@computerprogramminghelp.org</a>
+        <i class="icofont-phone"></i> +1 938 839 9820
+      </div>
+      <div class="social-links">
+        <a href="https://twitter.com/CodersForPay" class="twitter" target="_blank"><i class="icofont-twitter"></i></a>
+        <a href="https://www.facebook.com/ComputerProgrammingHelp" class="facebook" target="_blank"><i class="icofont-facebook"></i></a>
+        <a href="https://www.instagram.com/codersforpay" class="instagram" target="_blank"><i class="icofont-instagram"></i></a>
+        <!-- <a href="#" class="skype"><i class="icofont-skype"></i></a>
+        <a href="#" class="linkedin"><i class="icofont-linkedin"></i></i></a> -->
+      </div>
+    </div>
+  </section>
+
+  <!-- ======= Header ======= -->
+  <header id="header">
+    <div class="container d-flex">
+
+      <div class="logo mr-auto">
+        <!-- <h1 class="text-light"><a href="index.php"><span>CPH</span></a></h1> -->
+        <!-- Uncomment below if you prefer to use an image logo -->
+         <a href="index.php"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>
+      </div>
+
+      <nav class="nav-menu d-none d-lg-block">
+        <ul>
+          <li class="active"><a href="index.php">Home</a></li>
+
+          <li class="drop-down"><a href="#">Programming</a>
+            <ul>
+                <li class="drop-down"><a href="#">Languages</a>
+                    <ul>
+                        <li><a href="java-homework-assistance.php">Java</a></li>
+                        <li><a href="#">C++</a></li>
+                        <li><a href="#">C</a></li>
+                        <li><a href="python-homework-assistance.php">Python</a></li>
+                        <li><a href="python-homework-assistance.php">Ruby</a></li>
+                        <li><a href="#">PHP</a></li>
+                        <li><a href="#">Matlab</a></li>
+                        <li><a href="#">Haskel</a></li>
+                        <li><a href="#">Android</a></li>
+                        <li><a href="#">Kotlin</a></li>
+                        <li><a href="#">iOS</a></li>
+                        <li><a href="#">Arduino</a></li>
+                        <li><a href="#">Swift</a></li>
+                        <li><a href="#">C#</a></li>
+                        <li><a href="#">R</a></li>
+                        <li><a href="#">Visual Basic</a></li>
+                        <li><a href="#">Objective C</a></li>
+                        <li><a href="#">Javascript</a></li>
+                        <li><a href="#">AJAX</a></li>
+                        <li><a href="#">Node JS</a></li>
+                        <li><a href="#">Wordpress</a></li>
+                        <li><a href="#">php</a></li>
+                        <li><a href="#">CSS</a></li>
+                        <li><a href="#">Angular JS</a></li>
+                        <li><a href="#">Joomla</a></li>
+                        <li><a href="#">Prolog</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Website Design</a></li>
+                <li><a href="#">Data Structures</a></li>
+                <li><a href="#">Client-Server Programming</a></li>
+                <li><a href="#">Logic Programming</a></ABBR></li>
+                <li><a href="#">Socket Programming</a></li>
+                <li><a href="#">Pseudocode</a></li>
+                <li><a href="#">Code Optimization</a></li>
+                <li><a href="#">Memory Management</a></li>
+            </ul>
+          </li>
+
+
+          <li class="drop-down"><a href="#">Database</a>
+            <ul>
+              <li><a href="#">Database Homework Help</a></li>
+              <li><a href="#">Database Management System Assistance</a></li>
+              <li><a href="#">Advanced Database Management System</a></li>
+              <li class="drop-down"><a href="database-queries-homework-assistance.php">Database Queries</a>
+                  <ul>
+                      <li><a href="mysql-database-assignment-assistance.php">MySQL</a></li>
+                      <li><a href="#">Oracle</a></li>
+                      <li><a href="#">PostgreSQL</a></li>
+                      <li><a href="#">PL/SQL</a></li>
+                      <li><a href="#">NoSQL</a></li>
+                      <li><a href="#">MongoDB</a></li>
+                      <li><a href="#">MS Access</a></li>
+                  </ul>
+              </li>
+              <li><a href="#">NoSQL</a></li>
+              <li><a href="#">Microsoft Access</a></li>
+              <li><a href="database-homework-assistance.php">Database Design</a></li>
+              <li class="drop-down"><a href="#">Normalization</a>
+                  <ul>
+                      <li><a href="#">First Normal Form (1NF)</a></li>
+                      <li><a href="#">Second Normal Form (2NF)</a></li>
+                      <li><a href="#">Third Normal Form (3NF)</a></li>
+                      <li><a href="#">Fourth Normal Form (4NF)</a></li>
+                      <li><a href="#">Fifth Normal Form (5NF)</a></li>
+                      <li><a href="#">Sixth Normal Form (6NF)</a></li>
+                      <li><a href="#">Boyce–Codd Normal Form (BCNF)</a></li>
+                  </ul>
+              </li>
+              <li><a href="#">Distributed Database Homework Help</a></li>
+              <li><a href="#">Query Optimization</a></li>
+            </ul>
+          </li>
+
+          <li class="drop-down"><a href="#">Diagrams</a>
+            <ul>
+                <li><a href="#">Class Diagram</a></li>
+                <li><a href="#">UML Diagram</a></li>
+                <li class="drop-down"><a href="#">Data Flow Diagram (DFD)</a>
+                    <ul>
+                        <li><a href="#">Level 0 DFD (Context Diagram)</a></li>
+                        <li><a href="#">Level 1 DFD</a></li>
+                        <li><a href="#">Level 2 DFD</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">State Diagram</a></li>
+                <li><a href="#">Component Diagram</a></li>
+                <li><a href="#">Flow Chart</a></li>
+                <li><a href="#">Sequence Diagram</a></li>
+                <li><a href="#">Deployment Diagram</a></li>
+                <li><a href="#">Activity Diagram</a></li>
+                <li><a href="#">Context Diagram</a></li>
+                <li><a href="#">Gantt Chart</a></li>
+                <li><a href="#">Swimlane</a></li>
+            </ul>
+          </li>
+
+
+          <li class="drop-down"><a href="#">Networking</a>
+            <ul>
+              <li><a href="#">Cisco CCNA</a></li>
+              <li><a href="#">Cisco CCNP</a></li>
+            </ul>
+          </li>
+          <li class="drop-down"><a href="#">Other Services</a>
+            <ul>
+              <li><a href="#">Computer Science Homework Help</a></li>
+              <li><a href="#">Information Security Homework Help</a></li>
+              <li><a href="#">Cyber Security Assignment Help</a></li>
+              <li><a href="#">Data Mining Assignment</a></li>
+              <li><a href="#">Big Data Assignment Help</a></li>
+              <li><a href="#">Data Structures and Algorithms Homework Help</a></li>
+              <li><a href="#">Deadlocks</a></li>
+              <li><a href="#">Operating Systems</a></li>
+              <li><a href="#">File System</a></li>
+              <li><a href="#">Inter Process Communication (IPC)</a></li>
+              <li><a href="#">Scheduling</a></li>
+              <li><a href="#">Virtual Memory</a></li>
+              <li><a href="#">Research Papers</a></li>
+              <li><a href="#">Dessertations</a></li>
+              <li><a href="#">In-class Exam</a></li>
+              <li><a href="#">In-class Essay</a></li>
+              <li><a href="#">MyGust Exam</a></li>
+              <li><a href="#">Excel VBA</a></li>
+              <li><a href="#">Online Class Help</a></li>
+              <li><a href="#">Online Exams</a></li>
+            </ul>
+          </li>
+          <li><a href="#">Reviews</a></li>
+        </ul>
+      </nav><!-- .nav-menu -->
+
+    </div>
+  </header><!-- End Header -->
